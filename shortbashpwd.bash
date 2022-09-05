9125ef77817e7aaae2bd1f99b2ba3128f3d786c0
Nikita Ivanov <nikita.vyach.ivanov@gmail.com>
2022-09-05 17:10:18 +0500

    Use [[ instead of test

--- shortbashpwd.bash
+++ shortbashpwd.bash
@@ -28,23 +28,22 @@ _shortbashpwd_fmt() {
 }
 
 # Update PS1 when a directory is changed
-# The second condition allows a user to change PS1 on command line
+# The third condition allows a user to change PS1 on command line
 shortbashpwd() {
-	if [ -z "$1" ] || [ "$PWD" != "$_shortbashpwd_oldpwd" -a "$PS1" == "$_shortbashpwd_ps1" ]; then
+	if [[ -z $1 || ( "$PWD" != "$_shortbashpwd_oldpwd" && "$PS1" == '\[\e\]'* ) ]]; then
 		_shortbashpwd_fmt
-		PS1="${SHORT_BASH_PWD_PS1//\\[Ww]/$_shortbashpwd_pwd}"
+		PS1="\[\e\]${SHORT_BASH_PWD_PS1//\\[Ww]/$_shortbashpwd_pwd}"
 		_shortbashpwd_oldpwd="$PWD"
-		_shortbashpwd_ps1="$PS1"
 	fi
 }
 
 # Setup prompt command
 shortbashpwd-setup() {
-	[ -z "$SHORT_BASH_PWD_PS1" ] &&
+	[[ -z $SHORT_BASH_PWD_PS1 ]] &&
 		echo 'shortbashpwd: $SHORT_BASH_PWD_PS1 is not set' >&2 &&
 		return
 
-	[[ ! "$SHORT_BASH_PWD_PS1" =~ \\[Ww] ]] &&
+	[[ ! $SHORT_BASH_PWD_PS1 =~ \\[Ww] ]] &&
 		echo 'shortbashpwd: $SHORT_BASH_PWD_PS1 does not contain placeholder \w' >&2 &&
 		return
 
